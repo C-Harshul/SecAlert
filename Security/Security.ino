f578785cd17f80e9b49dc18179d0321c94ff2ca9
C-Harshul <59438961+C-Harshul@users.noreply.github.com>
2020-06-04 15:17:06 +0530

    Add files via upload

---
+++ Security/Security.ino
@@ -0,0 +1,53 @@
+#include <ESP8266WiFi.h>                                                // esp8266 library
+#include <FirebaseArduino.h>                                             // firebase library
+
+#define FIREBASE_HOST "security-9a9a0.firebaseio.com"                         // the project name address from firebase id
+#define FIREBASE_AUTH "kN824CJZUbbn7rYNPZMYmQ0IlTi2ZtpepHLcjEcv"                    // the secret key generated from firebase
+#define WIFI_SSID "Halul"                                          // input your home or public wifi name 
+#define WIFI_PASSWORD "1234567890"                                    //password of wifi ssid
+long duration;
+int distance;
+const int trigPin = 2;  //D4
+const int echoPin = 0;  //D3
+void setup() {
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+Serial.begin(115200); // Starts the serial communication
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);                                      //try to connect with wifi
+  Serial.print("Connecting to ");
+  Serial.print(WIFI_SSID);
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(500);
+  }
+  Serial.println();
+  Serial.print("Connected to ");
+  Serial.println(WIFI_SSID);
+  Serial.print("IP Address is : ");
+  Serial.println(WiFi.localIP());                                                      //print local IP address
+  Firebase.begin(FIREBASE_HOST, FIREBASE_AUTH);                                       // connect to firebase
+                                        //send initial string of led status
+}
+void loop(){
+// Clears the trigPin
+digitalWrite(trigPin, LOW);
+delayMicroseconds(2);
+ 
+// Sets the trigPin on HIGH state for 10 micro seconds
+digitalWrite(trigPin, HIGH);
+delayMicroseconds(10);
+digitalWrite(trigPin, LOW);
+
+// Reads the echoPin, returns the sound wave travel time in microseconds
+duration = pulseIn(echoPin, HIGH);
+
+// Calculating the distance
+distance= duration*0.034/2;
+// Prints the distance on the Serial Monitor
+Serial.print("Distance: ");
+Serial.println(distance);
+if(distance<10) 
+Firebase.setInt("Alert",distance);
+  
+}
+
